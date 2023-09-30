dc722de3fe2f4525181148e814a24598f8c00cd8
itspacrat <blakemichaelgaynor@gmail.com>
2023-09-29 22:02:57 -0700

    init `Ship`

---
+++ Scripts/Behaviors/Ship/ShipBehaviors.cs
@@ -0,0 +1,14 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace MonoGame_Core.Scripts
+{
+    public static class ShipBehaviors
+    {
+        
+    }
+}
---
+++ Scripts/Components/Galaxy/ShipData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame_Core.Scripts
+{
+    public class ShipData : Component
+    {
+        
+        public bool bobDir = false;
+        /// <summary>
+        /// The ship's onscreen position in fight mode
+        /// </summary>
+        public Vector2 shipPos;
+        
+        public ShipData(GameObject go, string name) : base(go, name)
+        {
+            shipPos = ((WorldObject)go).Transform.Position;
+
+        }
+    }
+}
