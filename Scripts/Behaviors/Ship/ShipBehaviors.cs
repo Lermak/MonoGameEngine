f10f79159730c81b9989022145c6735268211043
itspacrat <blakemichaelgaynor@gmail.com>
2023-09-30 13:33:13 -0700

    clean up `ShipBehaviors` and `ShipData`

--- Scripts/Behaviors/Ship/ShipBehaviors.cs
+++ Scripts/Behaviors/Ship/ShipBehaviors.cs
@@ -7,8 +7,26 @@ using System.Text;
 
 namespace MonoGame_Core.Scripts
 {
+    /// <summary>
+    /// To be used inside a Ship class
+    /// </summary>
     public static class ShipBehaviors
     {
-        
+        public static void ShootOnClick(float dt, GameObject go, Component[] c = null) {
+            Vector2 mousePos = InputManager.MousePos;
+            Transform shipTF = (Transform) go.GetComponent("transform");
+            ShipData shipData = (ShipData) go.GetComponent("ShipData");
+
+            if (InputManager.IsTriggered(InputManager.MouseKeys.Left)) {
+                if ( shipData.weaponAmmo >= 1) {
+                    
+                    // todo: do the shooting
+                    shipData.weaponAmmo -= 1;
+                } // else dont shoot
+            } // else dont do anything
+        }
+        public static void MoveWASD(float dt, GameObject go, Component[] c = null) {
+            // todo:
+        }
     }
 }
