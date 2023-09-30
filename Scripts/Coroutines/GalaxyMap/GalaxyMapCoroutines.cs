b5a9a5acc9dd22b2ef7b68a8701baf9544fc84ac
Rhen <rhenniles@yahoo.com>
2023-09-30 00:15:43 -0400

    adding sprites

---
+++ Scripts/Coroutines/GalaxyMap/GalaxyMapCoroutines.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame_Core.Scripts
+{
+    public static class GalaxyMapCoroutines
+    {
+        public static IEnumerator<bool> TravelToGalaxy(WorldObject ship, Transform galaxy)
+        {
+            Vector2 mv = (galaxy.Position - ship.Transform.Position);
+            mv.Normalize();
+            ship.RigidBody.MoveVelocity = mv * 3;
+            GalaxyData gd = ((GalaxyData)galaxy.GameObject.GetComponent("GalaxyData"));
+            ShipMarkerData sd = (ShipMarkerData)ship.GetComponent("ShipMarkerData");
+
+            while (Math.Abs(Vector2.Distance(ship.Transform.Position, galaxy.Position)) > ((CollisionCircle)(galaxy.GameObject.GetComponent("myCircle"))).Hypotenuse/2)
+            {
+                yield return false;
+            }
+            ship.RigidBody.MoveVelocity = new Vector2();
+            sd.InFlight = false;
+            sd.Row = gd.Row;
+            sd.idePos = ship.Transform.Position;
+            gd.GoShopping = true;
+            yield return true;
+        }
+    }
+}
---
+++ Scripts/GameObjects/GalaxyMap/ShipMarker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame_Core.Scripts
+{
+    public class ShipMarker : WorldObject
+    {
+        public ShipMarker(string texID, string name, Vector2 pos) : base(texID, name, new string[] { "testObject" }, pos, 2)
+        {
+            AddComponent(new ShipMarkerData(this, "ShipMarkerData"));
+            AddBehavior("GoToGalaxy", GalaxyMapBehaviors.TravelOnClick);
+            AddBehavior("IdleBob", GalaxyMapBehaviors.ShipMarkerIdleBob);
+        }
+    }
+}
