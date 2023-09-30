2a362d21e0ad8d3ab62d0bed1e86463fd98fb915
itspacrat <blakemichaelgaynor@gmail.com>
2023-09-30 14:41:59 -0700

    Scaffold `InventoryItem` + `InventoryGrid` data

---
+++ Scripts/Components/Galaxy/InventoryItemData.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace MonoGame_Core.Scripts
+{
+    public class InventoryItemData : Component
+    {
+        
+        // todo: scaff/actually put stuff here
+
+        public InventoryItemData(GameObject go, string name) : base(go, name)
+        {
+           
+        }
+    }
+}
