9c1338c06ffa934307f172a11a6786be61310d71
itspacrat <blakemichaelgaynor@gmail.com>
2023-09-30 15:56:52 -0700

    add check for empty inventory `IsDead`

--- Scripts/GameObjects/Ship.cs
+++ Scripts/GameObjects/Ship.cs
@@ -3,6 +3,7 @@ used when fighting i guess
 */
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -16,5 +17,28 @@ namespace MonoGame_Core.Scripts
             AddComponent(new ShipData(this, "ShipData"));
             AddComponent(new RigidBody(this,RigidBody.RigidBodyType.Dynamic));
         }
+        /// <summary>
+        ///  checks if there are no spaces filled in the ship
+        /// </summary>
+        /// <param name="inv"></param>
+        /// <returns></returns>
+        public bool IsDead(InventoryGridData inv) {
+            List<string> filled = new List<string> {};
+
+            foreach (string item in inv.cells) {
+                if (!(item == "")) {
+                    filled.Add(item);
+                }
+            }
+            if (filled.Count > 0) {
+                // if theres somethign in there ship is alive
+                return false;
+            } else {
+                // if its empty gg
+                return true;
+            }
+            
+        }
+
     }
 }
