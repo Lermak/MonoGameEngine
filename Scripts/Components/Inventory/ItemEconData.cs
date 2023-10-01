9e0efdf584d7bbd3d3f5472e16d443a7333940f3
Rhen <rhenniles@yahoo.com>
2023-09-30 23:43:08 -0400

    adding inventory data

---
+++ Scripts/Components/Inventory/ItemEconData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace MonoGame_Core.Scripts
+{
+    public class ItemEconData : Component
+    {
+        public int SalePrice;
+        public int PurchasePrice;
+        public string DisplayName;
+        public string DisplayTexId;
+
+        public ItemEconData(GameObject go, string name) : base(go, name)
+        {
+
+        }
+    }
+}
