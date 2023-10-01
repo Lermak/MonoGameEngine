404b0577e6adf40b5d0957e58bbe16335c7f96c4
itspacrat <blakemichaelgaynor@gmail.com>
2023-10-01 09:30:33 -0700

    add initial `PointToPlayer` + `EmitRandomBullet` behaviors

--- Scripts/Behaviors/Galaxy/ShipBehaviors.cs
+++ Scripts/Behaviors/Galaxy/ShipBehaviors.cs
@@ -12,6 +12,7 @@ namespace MonoGame_Core.Scripts
     /// </summary>
     public static class ShipBehaviors
     {
+        
         public static void ShootOnClick(float dt, GameObject go, Component[] c = null)
         {
             Vector2 mousePos = InputManager.MousePos;
@@ -32,7 +33,32 @@ namespace MonoGame_Core.Scripts
         /// <param name="go"></param>
         /// <param name="c"></param>
         public static void PointToPlayer(float dt, GameObject go, Component[] c=null) {
-            // todo: scaff
+            Transform t = ((WorldObject)go).Transform;
+            Transform playerTF = (Transform )c[0];
+
+            t.Radians = hf_Math.GetAngleDeg(playerTF.Position, t.Position) + 90 * (float)Math.PI / 180;
+            if (t.Parent != null)
+                t.Rotate(t.Radians - t.Parent.Radians);
+
+        }
+        /// <summary>
+        ///  spawns a bullet instance with parent's rotation at random intervals
+        ///  
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <param name="go"></param>
+        /// <param name="c"></param>
+        public static void EmitRandomBullet(float dt, GameObject go, Component[] c = null) {
+            Random rng = new Random();
+
+            Transform parentTF = ((WorldObject)go).Transform;
+            // on average 1 in every 120 frames the parent should shoot a bullet
+            int shoot = rng.Next(120);
+            // use any int here, 60 is a nice number right in the middle of 120
+            if (shoot == 60) {
+                SceneManager.CurrentScene.AddWorldObject(new Bullet("BulletTex", "", parentTF.Position, parentTF.RotationDegrees));
+            }
+
         }
 
     }
