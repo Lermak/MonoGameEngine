a6803f2e2ef79a0961d9157214614534bc418811
itspacrat <blakemichaelgaynor@gmail.com>
2023-10-08 12:27:34 -0700

    rename `Weapon` -> `WeaponRepr` to better reflect the responsibility of this class

--- Scripts/GameObjects/Galaxy/WeaponRepr.cs
+++ Scripts/GameObjects/Galaxy/WeaponRepr.cs
@@ -2,9 +2,9 @@
 using Microsoft.Xna.Framework;
 using MonoGame_Core.Scripts;
 
-public class Weapon: InventoryItem {
-    public Weapon(string name,string texID,Vector2 pos)
-    : base(name,texID,pos,InventoryItemShapeData.Shapes.Square) {
+public class WeaponRepr: WorldObject {
+    public WeaponRepr(string name,string texID,Vector2 pos)
+    : base(name,texID,new string[] {"weapon","repr"},pos,3) {
         AddComponent(new WeaponData(this,"WeaponData"));
         CoroutineManager.Add(Coroutines.Reload((WeaponData)GetComponent("WeaponData")),"Reload",1,true);
         AddBehavior("shoot",ShipBehaviors.ShootOnClick);
