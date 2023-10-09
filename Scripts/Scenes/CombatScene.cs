cf0ea04c9888cf120f02c3a4b9dd241876dacf25
itspacrat <blakemichaelgaynor@gmail.com>
2023-10-08 23:39:49 -0700

    add the grid in the combat scene

--- Scripts/Scenes/CombatScene.cs
+++ Scripts/Scenes/CombatScene.cs
@@ -55,20 +55,26 @@ namespace MonoGame_Core.Scripts
         {
 
             
+            PlayerShip player = (PlayerShip)InitWorldObject(new PlayerShip("Base", "playerShip", new Vector2(0, 0)));
 
-
-            if (Player.Ship != null)
+            if (Player.Ship == null)
+                InitWorldObject(new Player("Grid", "Grid", new Vector2(15, 8)));
+            else
             {
-
-                InventoryGridData inv = Player.Inventory;
-                //
-                // grab the weapons
-                List<InventoryItem> weapons =
-                inv.FetchItemsByType(ItemData.ItemTypes.Combat);
+                InitWorldObject(Player.Ship);
+                Player.Ship.SpriteRenderer.Cameras = new List<Camera>() { CameraManager.MainCamera };
+                
+                foreach(InventoryItem i in Player.Inventory.StoredItems)
+                {
+                    i.Restore();
+                }
             }
+
+            Player ship = Player.Ship;
+            ship.Transform.SetScale((float)0.05,(float)0.05);
             //
             // spawn in the player first
-            PlayerShip player = (PlayerShip)InitWorldObject(new PlayerShip("Base", "playerShip", new Vector2(0, 0)));
+            
             //
             // spawn in enemies, recursively find a good spawn point
             int max = 5;
