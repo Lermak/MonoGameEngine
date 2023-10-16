b837dc05c8e53b30f310d1b5d92f186f39b320f4
itspacrat <blakemichaelgaynor@gmail.com>
2023-10-16 12:19:06 -0700

    move `NextFloat` into `hf_Math`

--- Scripts/hf_Math.cs
+++ Scripts/hf_Math.cs
@@ -98,5 +98,21 @@ namespace MonoGame_Core.Scripts
             return new Vector2((float)(Math.Cos(DegToRad(degrees)) * (rotateAround.X - startPos.X) - Math.Sin(DegToRad(degrees)) * (rotateAround.Y - startPos.Y) + startPos.X),
             (float)(Math.Sin(DegToRad(degrees)) * (rotateAround.X - startPos.X) + Math.Cos(DegToRad(degrees)) * (rotateAround.Y - startPos.Y) + startPos.Y));
         }
+        /// <summary>
+        /// icnredibly hacky implementation of NextFloat() borrowed from stack https://stackoverflow.com/a/3365388/7416252
+        /// <br/>
+        /// <b>this is experimental and is in no way guaranteed to be safe</b> <i>(use at own risk, -Blake)</i>
+        /// </summary>
+        /// <param name="random"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static float NextFloat(Random random, int min, int max)
+        {
+            double mantissa = (random.NextDouble() * 2.0) - 1.0;
+            // choose -149 instead of -126 to also generate subnormal floats (*)
+            double exponent = Math.Pow(2.0, random.Next(min, max));
+            return (float)(mantissa * exponent);
+        }
     }
 }
